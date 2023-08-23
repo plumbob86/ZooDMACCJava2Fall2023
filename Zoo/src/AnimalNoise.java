2f0c413f54ed88bf9dbc8c355f97d034af181e5f
plumbob86 <plumbob86@users.noreply.github.com>
2023-08-23 14:55:04 -0500

    AnimalNoise added

---
+++ Zoo/src/AnimalNoise.java
@@ -0,0 +1,16 @@
+
+import model.Example;
+
+public class AnimalNoise {
+
+	public static void main(String[] args) {
+		Example example = new Example();
+		System.out.println(example.makeNoise());
+		
+		/*
+		 * Add a call to your animal below this comment.
+		 */
+
+	}
+
+}
